4cb6beb1e616c574eccb3e238f6310c179ca4f1c
unknown <avinash.sankajji94@gmai.com>
2021-10-26 21:34:54 +0530

    Welcome to lembda

---
+++ src/com/bridglab/lambda/UserRegistrationException.java
@@ -0,0 +1,9 @@
+package com.bridglab.lambda;
+
+public class UserRegistrationException extends Exception {
+
+    public UserRegistrationException(String message){
+        super(message);
+    }
+
+}
